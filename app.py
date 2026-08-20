cc655d4e7f6aaf70f3356a6cb5f5ad9c623455fe
juanav-tech <juanavasur@gmail.com>
2026-08-20 07:34:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,30 @@ st.image(image, caption="Interfaces Multimodales")
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es', texto)
+
+at.subhearder("Ahora usemos 2 columnas")
+
+col1, col2 = st.columns (2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experienica de usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp: 
+  st.write('Correcto!')
+
+with col2: 
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio("Que Modalidad es la principal en tu interfaz", ('Visual','auditiva','tactil'))
+  if modo == 'Visual':
+    st.write('La vista es fundamental para tu interfaz')
+  if modo == 'auditiva':
+    st.write('La audición es fundamental para tu interfaz')
+  if modo == 'Táctil':
+    st.write('El tacto es fundamental par tu interfaz')
+
+st.subheader("Uso de botones")
+if st.button('Presiona el botón'):
+  st.write('Gracias por presionar'
+           else:
+  st.write('No has presionado aún')
