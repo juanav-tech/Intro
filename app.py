683e7a0c3c5919602497120715d77d06d9331a1c
juanav-tech <juanavasur@gmail.com>
2026-08-20 07:17:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st<
+from PIL import Image
 
+st.title("La primera app de Juana en Streamlit")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open("vaca.jpg")
+st.image(image, caption="Interfaces Multimodales")
+*
 st.title("Mi primera app en streamlit")
