907a159dddad2e181daf0a5c9d5546a059f3271a
juanav-tech <juanavasur@gmail.com>
2026-08-20 07:21:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,6 @@ st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfac
 st.write("Facilmente puedo realizar backend y frontend")
 image = Image.open("vaca.jpg")
 st.image(image, caption="Interfaces Multimodales")
-*
-st.title("Mi primera app en streamlit")
+
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
